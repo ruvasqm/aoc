8103b2891dd7734d247c64b9eb862e7a149fdb8b
Ruben Vasquez <ruvasqm@gmail.com>
2023-11-10 22:49:04 -0400

    feat: add y2k15#1.2

--- rust/src/sol/y2k15.rs
+++ rust/src/sol/y2k15.rs
@@ -1,11 +1,30 @@
 pub fn day1(steps: &str) -> Result<String, &str> {
+    let mut b_flag = false;
+    let mut first_basement = "None";
+    let mut counter = 1;
     let result = steps.chars().fold(0, |acc, x| match x {
-        '(' => acc + 1,
-        ')' => acc - 1,
+        '(' => {
+            if !b_flag {
+                counter += 1;
+            }
+            acc + 1
+        }
+        ')' => {
+            if !b_flag && acc == 0 {
+                b_flag = true;
+            } else if !b_flag {
+                counter += 1;
+            }
+            acc - 1
+        }
         _ => acc,
     });
-
-    let answer = result.to_string();
+    let counter = counter.to_string();
+    first_basement = if b_flag { &counter } else { first_basement };
+    let answer = format!(
+        "final floor: {}\nfirst basement: {}",
+        result, first_basement
+    );
     Ok(answer)
 }
 
@@ -15,14 +34,25 @@ mod tests {
 
     #[test]
     fn test_day1() {
-        assert_eq!(day1("(())"), Ok("0".to_string()));
-        assert_eq!(day1("()()"), Ok("0".to_string()));
-        assert_eq!(day1("((("), Ok("3".to_string()));
-        assert_eq!(day1("(()(()("), Ok("3".to_string()));
-        assert_eq!(day1("))((((("), Ok("3".to_string()));
-        assert_eq!(day1("())"), Ok("-1".to_string()));
-        assert_eq!(day1("))("), Ok("-1".to_string()));
-        assert_eq!(day1(")))"), Ok("-3".to_string()));
-        assert_eq!(day1(")())())"), Ok("-3".to_string()));
+        assert_eq!(
+            day1("(()))"),
+            Ok("final floor: -1\nfirst basement: 5".to_string())
+        );
+        assert_eq!(
+            day1("))()"),
+            Ok("final floor: -2\nfirst basement: 1".to_string())
+        );
+        assert_eq!(
+            day1(")))"),
+            Ok("final floor: -3\nfirst basement: 1".to_string())
+        );
+        assert_eq!(
+            day1(")(()(()"),
+            Ok("final floor: 1\nfirst basement: 1".to_string())
+        );
+        assert_eq!(
+            day1(")"),
+            Ok("final floor: -1\nfirst basement: 1".to_string())
+        );
     }
 }
